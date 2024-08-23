99bb180136f979a36fa6b181b5c541be8cbd5efa
usmanramzan342 <usmantt.ramzan@gmail.com>
2024-08-23 14:27:37 +0500

    usman

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let table = +prompt("write the table you want to learn");
+let count = 1 ;
+let cat = table * count ;
+document.write("multiplication table of "  +  table + "<br />"); 
+document.write(table   +" "+   " x "  +"  "+    count   +" "+   " = "   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
+document.write(table   +" "+   "x"  +"  "+    count   +" "+   "="   +" "+   table*count++  +" "+   "<br />");
